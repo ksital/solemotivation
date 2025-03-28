66072cea4db572c7aa720dccab1c71b5f255381a
Keya Sital <keyadevi.sital@dcmail.ca>
2025-03-28 08:22:48 -0400

    functions.php
    Enqueue Google Fonts (Montserrat) for the form

--- themes/no-code-theme/functions.php
+++ themes/no-code-theme/functions.php
@@ -10,3 +10,16 @@
 
 // Filters.
 require_once get_theme_file_path( 'inc/hooks.php' );
+
+// Enqueue Google Fonts (Montserrat)
+function solemotivation_enqueue_styles() {
+    // Load Montserrat font
+    wp_enqueue_style(
+        'montserrat-font',
+        'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap',
+        array(),
+        null
+    );
+}
+add_action('wp_enqueue_scripts', 'solemotivation_enqueue_styles');
+
